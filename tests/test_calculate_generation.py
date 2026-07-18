2c83d6fef885414d4427bf061f728c2224ad33e9
Nguyen Hoang Minh Huy <nguyenhoangminhhuy23022009@gmail.com>
2026-07-18 19:52:51 +0700

    fix: test calculate generation

--- tests/test_calculate_generation.py
+++ tests/test_calculate_generation.py
@@ -5,48 +5,25 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
 from src.calculator import calculate_generation_kwh
 
-actual=calculate_generation_kwh(system_kwp=5,solar_radiation=5.2,days_in_month=30,efficiency_factor=0.79)
-expected=616.2
+import pytest
 
-print("Expected:",expected)
+def test_generation_normal_case():
+    result=calculate_generation_kwh(system_kwp=5,solar_radiation=5.2,days_in_month=30,efficiency_factor=0.8051)
+    assert result==pytest.approx(627.978,abs=0.01)
 
-print("Actual:",actual)
-
-if abs(actual-expected)<0.01:
-    print("Result: PASS")
-else:
-    print("Result: FAIL")
-
-invalid_test_cases=[
-    {
-        "name":"System capacity is zero",
-        "system_kwp":0,
-        "solar_radiation":5.2,
-        "days_in_month":30,
-        "efficiency_factor":0.79
-    },
-    {
-        "name":"Negative solar radiation",
-        "system_kwp":5,
-        "solar_radiation":-6736,
-        "days_in_month":30,
-        "efficiency_factor":0.79
-    },
-    {
-        "name":"PR greater than 1",
-        "system_kwp":5,
-        "solar_radiation":5.2,
-        "days_in_month":30,
-        "efficiency_factor":3.6
-    }
-]
-
-for test_case in invalid_test_cases:
-    print("\nTest:", test_case["name"])
+def test_generation_zero_solar_radiation():
+    result=calculate_generation_kwh(system_kwp=5,solar_radiation=0,days_in_month=30,efficiency_factor=0.8051)
+    assert result==0
     
-    try:
-        calculate_generation_kwh(test_case["system_kwp"],test_case["solar_radiation"],test_case["days_in_month"],test_case["efficiency_factor"])
-        print("Result: FAIL")
-    except ValueError as e:
-        print("Error:",e)
-        print("Result: PASS")
+def test_generation_invalid_solar_radiation():
+    with pytest.raises(ValueError):
+        calculate_generation_kwh(system_kwp=5,solar_radiation=-5.2,days_in_month=30,efficiency_factor=0.8051)
+        
+def test_generation_invalid_system_capacity():
+    with pytest.raises(ValueError):
+        calculate_generation_kwh(system_kwp=0,solar_radiation=5.2,days_in_month=30,efficiency_factor=0.8051)
+        
+def test_generation_invalid_efficiency_factor():
+    with pytest.raises(ValueError):
+        calculate_generation_kwh(system_kwp=5,solar_radiation=5.2,days_in_month=30,efficiency_factor=1.5)
+        
